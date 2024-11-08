7f9f8d1aff8b1cd77266a08113b68add2b4d2271
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-11-08 23:12:08 +0300

    Splash view ui

---
+++ lib/core/utils/assets_data.dart
@@ -0,0 +1,13 @@
+// ignore_for_file: prefer_single_quotes
+class AssetsData {
+  AssetsData._();
+  
+  /// Assets for assetsDataImagesLogo
+  /// assets/images/Logo.png
+  static const String assetsDataImagesLogo = "assets/images/Logo.png";
+
+  /// Assets for assetsDataImagesTestImage
+  /// assets/images/test_image.png
+  static const String assetsDataImagesTestImage = "assets/images/test_image.png";
+}
+
