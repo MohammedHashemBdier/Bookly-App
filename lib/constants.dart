4bcef18247713b4961478e1436cbaf6edb72da8b
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-11-08 22:36:14 +0300

    Adjust some things

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
 
-const kPrimaryColor = Color(0xff100B20);
+const kPrimaryColor = Color(0xFF100B20);
 const kTranstionDuration = Duration(milliseconds: 250);
 const kGtSectraFine = 'GT Sectra Fine';
