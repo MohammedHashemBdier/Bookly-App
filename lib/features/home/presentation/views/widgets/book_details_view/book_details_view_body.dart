a9f75134f0de7478e9eee9f71c7e3fb9da3b460b
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-11-16 20:39:42 +0300

    add title & subtitle to book details

--- lib/features/home/presentation/views/widgets/book_details_view/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view/book_details_view_body.dart
@@ -1,3 +1,4 @@
+import 'package:bookly_app/core/utils/app_styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_view/custom_book_details_app_bar.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -18,9 +19,29 @@ class BookDetailsViewBody extends StatelessWidget {
           const CustomBookDetailsAppBar(),
           Padding(
             padding: EdgeInsets.symmetric(
-              horizontal: width * .17,
+              horizontal: width * .2,
             ),
             child: const CustomBookImage(),
+          ),
+          const SizedBox(
+            height: 43,
+          ),
+          const Text(
+            "The Jungel Book",
+            style: AppStyles.textStyle20GtSectraFine,
+          ),
+          const SizedBox(
+            height: 6,
+          ),
+          Opacity(
+            opacity: .7,
+            child: Text(
+              "J.K Roling",
+              style: AppStyles.textStyle18.copyWith(
+                fontWeight: FontWeight.w500,
+                fontStyle: FontStyle.italic,
+              ),
+            ),
           ),
         ],
       ),
