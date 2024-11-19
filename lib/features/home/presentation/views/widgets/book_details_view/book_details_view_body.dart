fffa1391439c4d963996132280a691b22da03a6d
Mohammed Hashem Bdier <mhb20033@gmail.com>
2024-11-19 19:48:40 +0300

    Create similar books list view

--- lib/features/home/presentation/views/widgets/book_details_view/book_details_view_body.dart
+++ lib/features/home/presentation/views/widgets/book_details_view/book_details_view_body.dart
@@ -1,6 +1,7 @@
 import 'package:bookly_app/core/utils/app_styles.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_view/books_action.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_details_view/custom_book_details_app_bar.dart';
+import 'package:bookly_app/features/home/presentation/views/widgets/book_details_view/similar_Books_list_view.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/book_rating.dart';
 import 'package:bookly_app/features/home/presentation/views/widgets/custom_book_image.dart';
 import 'package:flutter/material.dart';
@@ -16,49 +17,74 @@ class BookDetailsViewBody extends StatelessWidget {
         vertical: 20,
         horizontal: 30,
       ),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: width * .2,
+      child: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(
+            child: Column(
+              children: [
+                const CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: width * .2,
+                  ),
+                  child: const CustomBookImage(),
+                ),
+                const SizedBox(
+                  height: 43,
+                ),
+                const Text(
+                  "The Jungel Book",
+                  style: AppStyles.textStyle20GtSectraFine,
+                ),
+                const SizedBox(
+                  height: 6,
+                ),
+                Opacity(
+                  opacity: .7,
+                  child: Text(
+                    "J.K Roling",
+                    style: AppStyles.textStyle18.copyWith(
+                      fontWeight: FontWeight.w500,
+                      fontStyle: FontStyle.italic,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 18,
+                ),
+                const BookRating(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                ),
+                const SizedBox(
+                  height: 37,
+                ),
+                const Padding(
+                  padding: EdgeInsets.symmetric(
+                    horizontal: 8,
+                  ),
+                  child: BooksAction(),
+                ),
+                const SizedBox(
+                  height: 50,
+                ),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    "You can also like",
+                    style: AppStyles.textStyle14.copyWith(
+                      fontWeight: FontWeight.w600,
+                    ),
+                  ),
+                ),
+                const SizedBox(
+                  height: 16,
+                ),
+                const SimilarBooksListView(),
+                const SizedBox(
+                  height: 40,
+                ),
+              ],
             ),
-            child: const CustomBookImage(),
-          ),
-          const SizedBox(
-            height: 43,
-          ),
-          const Text(
-            "The Jungel Book",
-            style: AppStyles.textStyle20GtSectraFine,
-          ),
-          const SizedBox(
-            height: 6,
-          ),
-          Opacity(
-            opacity: .7,
-            child: Text(
-              "J.K Roling",
-              style: AppStyles.textStyle18.copyWith(
-                fontWeight: FontWeight.w500,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-          ),
-          const SizedBox(
-            height: 18,
-          ),
-          const BookRating(
-            mainAxisAlignment: MainAxisAlignment.center,
-          ),
-          const SizedBox(
-            height: 37,
-          ),
-          const Padding(
-            padding: EdgeInsets.symmetric(
-              horizontal: 8,
-            ),
-            child: BooksAction(),
           ),
         ],
       ),
